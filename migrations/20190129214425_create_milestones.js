21245c7b45cbaf46e61f37343f3b9f83826e2281
Christina <christina.hsu.ca@gmail.com>
2019-01-30 01:09:14 +0000

    redid the famous people part and added the FK to the table

---
+++ migrations/20190129214425_create_milestones.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table) {
+      table.increments('id');
+      table.string('description').notNullable();
+      table.string('date_achieved').notNullable();
+      table.timestamps();
+    })
+    ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+     knex.schema.dropTable('milestones')
+    ]);
+};
